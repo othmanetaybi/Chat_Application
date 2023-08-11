8a7b93b39887fcfc8127753d515725af7c845a73
othmanetaybi <othmanetaybi>
2023-08-11 20:24:55 +0100

    first commit

---
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+</div>
